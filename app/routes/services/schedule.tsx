3eaf9671959bfece3dd51551694bf74742f816ea
Tayyaba <tayyabarmemon13@gmail.com>
2026-07-27 15:14:36 -0500

    Replace Google Calendar with Coming Soon message

--- app/routes/services/schedule.tsx
+++ app/routes/services/schedule.tsx
@@ -12,18 +12,13 @@ export default function Schedule() {
   return (
     <div>
       <h1 className="text-4xl sm:text-5xl md:text-6xl text-primary mt-12 mx-32 font-[Bebas_Neue] text-center">
-        Google Calendar
+        Schedule
       </h1>
 
-      <div className="flex justify-center mt-10">
-        <iframe
-          src="https://calendar.google.com/calendar/embed?src=c_83ffed25b11da3ba07bde172418c02218857c61ea8fc57600732255b0d86281a%40group.calendar.google.com&ctz=America%2FChicago"
-          style={{ border: 0 }}
-          width="800"
-          height="600"
-          frameBorder="0"
-          scrolling="no"
-        />
+      <div className="flex justify-center items-center mt-10">
+        <p className="text-2xl md:text-4xl text-primary font-[Bebas_Neue]">
+          Coming Soon
+        </p>
       </div>
     </div>
   );
