4f40deb9d9ab6e879a55efdc56230359c24e9ad0
Tayyaba <tayyabarmemon13@gmail.com>
2026-07-27 17:50:00 -0500

    Remove unknown.jpg image from outreach.tsx

--- app/routes/involved/outreach.tsx
+++ app/routes/involved/outreach.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React from "react";
-import absImg from '../../images/about/providers/unknown.jpg'
 import type { Route } from "../+types/home";
 import { Button } from "~/components/ui/button"
 
@@ -16,11 +15,6 @@ export default function Outreach() {
   return (
     <div className="px-4 md:px-8 pb-12">
         <div className="flex flex-col md:flex-row items-center pt-8 md:pt-16 gap-6 md:gap-0 md:-space-x-10">
-            <img
-                src={absImg}
-                alt="Abstract Image"
-                className="w-48 h-48 sm:w-64 sm:h-64 md:w-1/4 md:h-auto rounded-md md:ml-30 object-cover"
-            />
             <div className="w-full md:pl-16">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl text-primary mt-4 md:mt-16 mx-4 md:mx-32 font-[Bebas_Neue] text-center">
                   Community Outreach Reports
@@ -29,7 +23,7 @@ export default function Outreach() {
                   Coming soon!
                 </p>
                 <div className="flex justify-center mt-8 md:mt-10 px-4 sm:px-8">
-                    <Button size="lg" variant="secondary" className="text-base sm:text-lg px-6 sm:px-8 md:px-12 py-6 md:py-7 bg-primary text-white rounded-lg hover:bg-[#dd73b5] transition-all duration-200 cursor-pointer opacity-50 pointer-events-none">
+                    <Button size="lg" variant="secondary" className="text-base sm:text-lg px-6 sm:px-8 md:px-12 py-6 md:py-7 bg-primary text-white rounded-lg hover:bg-[#dd73b5] transition-all duration-200 cursor-pointer">
                         Reports Coming Soon
                     </Button>
                 </div>
