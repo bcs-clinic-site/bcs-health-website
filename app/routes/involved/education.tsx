5e35aae90bde0a30c6d9858da8aac1f04b8c7d87
Tayyaba <tayyabarmemon13@gmail.com>
2026-07-27 17:51:54 -0500

    Remove all unknown.jpg references from all files

--- app/routes/involved/education.tsx
+++ app/routes/involved/education.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React from "react";
-import absImg from '../../images/about/providers/unknown.jpg'
 import type { Route } from "../+types/home";
 import { Button } from "~/components/ui/button"
 import getPictures from "~/components/getPictures";
@@ -18,17 +17,12 @@ export default function Education() {
   return (
     <div className="px-4 md:px-8 pb-12">
         <div className="flex flex-col md:flex-row items-center pt-8 md:pt-16 gap-6 md:gap-0 md:-space-x-10">
-            <img
-                src={absImg}
-                alt="Abstract Image"
-                className="w-48 h-48 sm:w-64 sm:h-64 md:w-1/4 md:h-auto rounded-md md:ml-30 object-cover"
-            />
             <div className="w-full md:pl-16">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl text-primary mt-4 md:mt-16 mx-4 md:mx-32 font-[Bebas_Neue] text-center">
                   Education Reports
                 </h1>
                 <p className="text-base sm:text-lg text-primary text-center max-w-4xl mx-auto mt-6 px-4 sm:px-6">
-                  We believe in transparency and accountability by sharing the impact of our education initiatives. Each event is documented in a detailed report highlighting activities, outcomes, and community impact.<br /> <br />You can browse all past reports through our Google Folder or view the most recent report for each event using the buttons below.
+                  We believe in transparency and accountability by sharing the impact of our education initiatives. Each event is documented in a detailed report highlighting activities, outcomes, and insights.
                 </p>
                 <div className="flex flex-col sm:flex-row justify-center gap-4 sm:gap-6 md:gap-8 mt-8 md:mt-10 px-4 sm:px-8">
                     <a href="https://drive.google.com/drive/folders/1kkpFTyETH75C6VxcjKqN2jhVylI_RZWZ?usp=sharing" target="_blank" rel="noopener noreferrer" className="w-full sm:w-auto">
